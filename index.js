400e0ff780feb206fd72778c1e61b480fff6ca5c
sabby155 <nebula155@gmail.com>
2019-04-23 18:05:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+let dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4";
+
+
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerRight(){
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
